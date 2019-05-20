638ce64c29a2e0feee44171f0b4cf775167ff731
cesar.junior <cesar.junior@basis.com.br>
2019-05-20 19:00:31 -0300

    inicio da tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,189 @@
 import 'package:flutter/material.dart';
-import 'RandomWords.dart';
-
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-       title: 'Startup Name Generator',
-       home: RandomWords(),
+      title: 'Welcome to Flutter',
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("lib/images/background.png"),
+                fit: BoxFit.cover)),
+        child: Scaffold(
+          resizeToAvoidBottomPadding: false,
+          backgroundColor: Colors.transparent,
+          body: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Divider(height: 70, color: Colors.transparent),
+                    Image.asset(
+                      'lib/images/igor_icon.png',
+                      width: 200,
+                      height: 60,
+                      fit: BoxFit.fill,
+                    ),
+                    Divider(),
+                    Image.asset(
+                      'lib/images/botao_google.png',
+                      width: 220,
+                      height: 50,
+                      fit: BoxFit.fill,
+                    ),
+                    Divider(height: 5, color: Colors.transparent),
+                    Image.asset(
+                      'lib/images/botao_facebook.png',
+                      width: 220,
+                      height: 50,
+                      fit: BoxFit.fill,
+                    ),
+                    Divider(height: 20, color: Colors.transparent),
+                    Text(
+                      'OU',
+                      style: TextStyle(
+                          fontFamily: 'Fira Sans',
+                          color: Colors.white,
+                          fontSize: 20),
+                    ),
+                    Column(children: <Widget>[
+                      TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.text,
+                        style: new TextStyle(color: Colors.white, fontSize: 15),
+                        decoration: InputDecoration(
+                            labelText: "E-mail",
+                            labelStyle: TextStyle(color: Colors.white)),
+                      ),
+                      Divider(),
+                      TextFormField(
+                        autofocus: true,
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                        style: new TextStyle(color: Colors.white, fontSize: 15),
+                        decoration: InputDecoration(
+                            labelText: "Senha",
+                            labelStyle: TextStyle(color: Colors.white)),
+                      ),
+                    ]),
+                  ])),
+        ),
+      ),
     );
   }
 }
+/*Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          */ /*1*/ /*
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              */ /*2*/ /*
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+        */ /*3*/ /*
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        Text('41'),
+      ],
+    ),
+  );
+
+  Widget buttonSection = Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.call, color: Colors.blue),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Text(
+                'CALL',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.near_me, color: Colors.blue),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Text(
+                'ROUTE',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.share, color: Colors.blue),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Text(
+                'SHARE',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
+    ),
+  );*/
